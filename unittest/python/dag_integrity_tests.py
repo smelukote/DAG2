8553408d116c5946ba5e5dad0438a5eb6199cca7
smelukote <smelukote@gmail.com>
2021-09-25 13:31:56 -0700

    addeda new requirement file

--- unittest/python/dag_integrity_tests.py
+++ unittest/python/dag_integrity_tests.py
@@ -1,33 +1,37 @@
+"""
+DAG Integrity Tests
+"""
+import os
 import unittest
-from airflow.models import DagBag
+from airflow.models import DagBag, Variable
+ 
 
+class TestDags(unittest.TestCase):
+    """DAG Test Case"""
 
-class TestDagIntegrity(unittest.TestCase):
-    LOAD_SECOND_THRESHOLD = 2
+    LOAD_THRESHOLD_SECONDS = 2
 
     def setUp(self):
+        os.system("airflow initdb")
+        for v in ["sample_var",
+        "sample_var2", 
+        "slack_channel",
+        "slack_token",
+        "slack_username",
+        "slack_icon_url"]:
+            Variable.set(v,v)
         self.dagbag = DagBag()
 
-    def test_import_dags(self):
+    def test_dags_syntax(self):
+        """Assert DAG bag load correctly"""
+        for key in self.dagbag.dags:
+            print(key)
         self.assertFalse(
             len(self.dagbag.import_errors),
-            'DAG import failures. Errors: {}'.format(
-                self.dagbag.import_errors
-            )
-        )
+            f"DAG import errors. Errors: {self.dagbag.import_errors}")
 
-    def test_import_time(self):
-        stats = self.dagbag.dagbag_stats
-        slow_dags = list(filter(lambda d: d.duration > self.LOAD_SECOND_THRESHOLD, stats))
-        res = ', '.join(map(lambda d: d.file[1:], slow_dags))
 
-        self.assertEquals(0, len(slow_dags),
-                          'The following files take more than {threshold}s to load: {res}'.format(
-                              threshold=self.LOAD_SECOND_THRESHOLD, res=res)
-                          )
-
-    def test_alert_email_present(self):
-        for dag_id, dag in self.dagbag.dags.items():
-            emails = dag.default_args.get('email', [])
-            msg = 'Alert email not set for DAG {id}'.format(id=dag_id)
-            self.assertIn('hello@world.com', emails, msg)
+if __name__ == '__main__':
+    
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestDags)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
